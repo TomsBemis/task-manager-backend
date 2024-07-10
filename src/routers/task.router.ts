3c250ef86ba5ab3c2b39f06bebbb2ff9d963d600
TomsBemis <toms.bemis@vasscompany.com>
2024-07-10 12:02:00 +0300

    TM-28 Create task table with dummy data

--- src/routers/task.router.ts
+++ src/routers/task.router.ts
@@ -1,8 +1,54 @@
 import { Router } from 'express';
+import { initialTasks, initialTaskTypes, initialTaskStatuses } from "../initialTaskData";
 import asyncHandler from 'express-async-handler';
-import { TaskModel } from '../models/task.model';
+import { Task, TaskModel } from '../models/task.model';
+import { TaskTypeModel, TaskStatusModel, Option } from '../models/option.model';
 
 const router = Router();
+
+router.get("/initialize", asyncHandler(
+    async (request, response) => {
+
+        // Initialize task types
+        if(!await TaskTypeModel.countDocuments()) 
+            await TaskTypeModel.create(initialTaskTypes);
+        let taskTypes = await TaskTypeModel.find();
+
+        // Initialize task statuses
+        if(!await TaskStatusModel.countDocuments()) 
+            await TaskStatusModel.create(initialTaskStatuses);
+        let taskStatuses = await TaskStatusModel.find();
+
+        if(!await TaskModel.countDocuments()) {
+
+            let tasks : Task[] = [];
+            let taskType : Option | undefined;
+            let taskStatus : Option | undefined;
+            initialTasks.forEach(initialTask => {
+                taskType = taskTypes.find( taskType => 
+                    taskType.value == initialTask.type
+                );
+                taskStatus = taskStatuses.find( taskStatus => 
+                    taskStatus.value == initialTask.status
+                );
+                if(!taskType || !taskStatus) throw Error("");
+
+                tasks.push({
+                    id: 0,
+                    title: initialTask.title,
+                    description: initialTask.description,
+                    type: taskType,
+                    status: taskStatus,
+                    createdOn: new Date(initialTask.createdOn),
+                    modifiedOn: new Date(initialTask.modifiedOn)
+                });
+            });
+            await TaskModel.create(tasks);
+        };        
+
+        response.send("Database is intialized")
+    }
+));
 
 router.get("/", asyncHandler(async (request, response) => {
     const tasks = await TaskModel.find();
